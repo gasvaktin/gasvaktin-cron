78ae96dbe306a208212e250e70f4809d91a294ba
Loknar <svefgud@gmail.com>
2017-11-09 00:00:04 +0000

    missing space

--- gasvaktin_cron.sh
+++ gasvaktin_cron.sh
@@ -19,7 +19,7 @@ ${python} gasvaktin_updater.py  > logs/run_${MY_TIMESTAMP}.log 2>&1
 
 # update trends
 cd ${gasvaktin_repo_path}/scripts
-${python} trends.py|| ${python} slack_msg.py "trends failure: ${MY_TIMESTAMP}"
+${python} trends.py || ${python} slack_msg.py "trends failure: ${MY_TIMESTAMP}"
 cd ${gasvaktin_cron_repo_path}
 ${python} gasvaktin_trends_updater.py  > logs/run_${MY_TIMESTAMP}_trends.log 2>&1
 
